d88e1c0b307a2959ef7aae4e335a01ccfcab82df
callam <callam@callamdelaney.me>
2023-02-02 10:33:59 +0000

    the problem

---
+++ schema.py
@@ -0,0 +1,24 @@
+import asyncio
+from typing import AsyncGenerator
+
+
+import strawberry
+
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def hello(self) -> str:
+        return "world"
+
+
+@strawberry.type
+class Subscription:
+    @strawberry.subscription
+    async def count(self, target: int = 100) -> AsyncGenerator[int, None]:
+        for i in range(target):
+            yield i
+            await asyncio.sleep(0.5)
+
+
+schema = strawberry.Schema(query=Query, subscription=Subscription)
